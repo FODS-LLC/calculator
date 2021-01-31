47b119a0ccb63d4042a5d0dce435d520b5920c97
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-01-30 21:31:28 -0700

    ROI

--- store/index.js
+++ store/index.js
@@ -18,8 +18,8 @@ export const state = () => ({
 
 export const mutations = {
   printToConsole(state, newForm) {
-    console.log("this.$store.state.formData.entrance_length")
-    console.log(this.$store.state.formData.entrance_length)
+    console.log("$store.state.formData.entrance_length")
+    console.log($store.state.formData.entrance_length)
     console.log('Form')
     console.log(newForm)
     console.log('State')
