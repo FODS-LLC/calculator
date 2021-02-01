1424694b0552f20651630407fcbd1a39bc8e2a63
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-02-01 10:42:09 -0700

    Add dollar signs

--- components/output/Calculations.vue
+++ components/output/Calculations.vue
@@ -176,15 +176,15 @@ export default {
       return [
         [
           '3 Years',
-          `${savingsYear3.toLocaleString("en-US")} USD`,
+          `\$ ${savingsYear3.toLocaleString("en-US")} USD`,
         ],
         [
           '5 Years',
-          `${savingsYear5.toLocaleString("en-US")} USD`,
+          `\$ ${savingsYear5.toLocaleString("en-US")} USD`,
         ],
         [
           '10 Years',
-          `${savingsYear10.toLocaleString("en-US")} USD`,
+          `\$ ${savingsYear10.toLocaleString("en-US")} USD`,
         ],
       ]
     },
@@ -219,27 +219,27 @@ export default {
       return [
         [
           '1x / year',
-          `${acceleratorYear3x1.toLocaleString("en-US")} USD`,
-          `${acceleratorYear5x1.toLocaleString("en-US")} USD`,
-          `${acceleratorYear10x1.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear3x1.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear5x1.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear10x1.toLocaleString("en-US")} USD`,
         ],
         [
           '3x / year',
-          `${acceleratorYear3x3.toLocaleString("en-US")} USD`,
-          `${acceleratorYear5x3.toLocaleString("en-US")} USD`,
-          `${acceleratorYear10x3.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear3x3.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear5x3.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear10x3.toLocaleString("en-US")} USD`,
         ],
         [
           '5x / year',
-          `${acceleratorYear3x5.toLocaleString("en-US")} USD`,
-          `${acceleratorYear5x5.toLocaleString("en-US")} USD`,
-          `${acceleratorYear10x5.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear3x5.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear5x5.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear10x5.toLocaleString("en-US")} USD`,
         ],
         [
           '10x / year',
-          `${acceleratorYear3x10.toLocaleString("en-US")} USD`,
-          `${acceleratorYear5x10.toLocaleString("en-US")} USD`,
-          `${acceleratorYear10x10.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear3x10.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear5x10.toLocaleString("en-US")} USD`,
+          `\$ ${acceleratorYear10x10.toLocaleString("en-US")} USD`,
         ],
       ]
     },
