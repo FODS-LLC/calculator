c925e6c8f55f6725d8e69308f151c4bf2d1bab9c
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-02-01 15:25:25 -0700

    add Helvetica

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -3,11 +3,11 @@
     <img
       src="/logo/FODS-LOG2O.png"
       alt="Logo"
-      class="object-contain mx-4 my-auto w-logo h-full cursor-pointer"
+      class="object-contain mx-4 my-auto sm:w-logo w-24 h-full cursor-pointer"
       @click="goToHomePage()"
     />
     <div class="self-center xl:m-auto my-auto sm:ml-0 sm:mr-auto text-center sm:text-left">
-      <h1 class="font-bold font-bebas-neue-bold md:leading-0px pt-20px text-mid-grey text-40 sm:text-64 xl:text-96 leading-none">
+      <h1 class="font-bold font-bebas-neue-bold md:leading-0px pt-20px text-mid-grey text-40 sm:text-64 text-32 sm:text-center text-left xl:text-96 leading-none">
         ROI Calculator
       </h1>
     </div>
