acc1ac4f6e2095317c22145da5e1191016e34df2
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-02-01 10:40:42 -0700

    Header

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="grid w-full grid-cols-header">
+  <div class="flex w-full">
     <img
       src="/logo/FODS-LOG2O.png"
       alt="Logo"
-      class="col-span-1 mx-2 my-6 w-logo cursor-pointer"
+      class="col-span-1 object-contain mx-2 my-6 w-logo cursor-pointer"
       @click="goToHomePage()"
     />
-    <div class="self-center xl:m-auto sm:ml-0 sm:mr-auto col-span-2 text-center">
+    <div class="self-center xl:m-auto sm:ml-0 sm:mr-auto text-center">
       <h1 class="font-bold font-bebas-neue-bold text-mid-grey text-40 sm:text-64 lg:text-96">
         ROI Calculator
       </h1>
