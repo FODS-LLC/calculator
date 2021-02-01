f2df6f2e119fe679ec6bbc0c0f6b0cbc5572c495
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-02-01 14:11:52 -0700

    add Helvetica

--- components/common/GenericForm.vue
+++ components/common/GenericForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="mb-4 font-bold text-center text-22 font-nexa-bold sm:text-left">
+    <h2 class="mb-4 font-bold text-center text-22 font-helvetica-neue sm:text-left">
       {{ label }}
     </h2>
     <form class="flex flex-wrap items-end justify-between">
