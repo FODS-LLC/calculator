92c1bded373d3492f8c1fa14434befc2f522eab6
Kevin Montgomery <kmontgomery@Kevins-MacBook-Pro.local>
2021-02-17 15:59:35 -0700

    hpf

--- pages/authorize.vue
+++ pages/authorize.vue
@@ -55,7 +55,7 @@ export default {
   methods: {
       async authorize() {
         this.error = ''
-        if (this.data.message.length > 0) {
+        if (this.data.message.length > 0) {console.log(this)}
           try {
             await this.$axios.post('https://getfods-api.herokuapp.com/checkCode', {
               code: this.code,
@@ -67,7 +67,7 @@ export default {
             console.log('err', err)
             this.error = 'Incorrect code.'
           }
-      }
+      
     },
   },
 }
